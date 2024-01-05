a3777bdbcfce2fbbd8a4ce5eaf6b08931740b013
Constancio Molinengo <constancio.molinengo@outlook.com>
2024-01-05 12:22:06 -0300

    [COTI-JOKE] add implementations in memory repo

--- adapters/src/repositories/memory/memory_joke_repository.py
+++ adapters/src/repositories/memory/memory_joke_repository.py
@@ -1,9 +1,17 @@
+import random
 from core.src.repository import JokeRepository
+from core.src.models import Joke
 
 class MemoryJokeRepository(JokeRepository):
-    def get_joke():
-        pass
+    def __init__(self):
+        self.jokes = [] 
+
+    def get_joke(self):
+        if self.jokes:
+            return random.choice(self.jokes)
+        else:
+            return None 
     
-    def add_joke():
-        pass
+    def add_joke(self, joke: Joke):
+        self.jokes.append(joke)
 
--- core/tests/usecases/test_get_joke.py
+++ core/tests/usecases/test_get_joke.py
@@ -1,9 +1,12 @@
 from typing import Callable
 from core.src.repository import JokeRepository
 from adapters.src.repositories import MemoryJokeRepository
+from core.src.usecases import GetJoke
 
 class TestGetJoke:
-    def test_get_joke(self, joke_factory: callable) -> None:
-        joke_repository: JokeRepository = MemoryJokeRepository()
-        joke = joke_factory()
-        pass
+    def test_get_joke(self, joke_factory: Callable) -> None:
+        joke_repository: JokeRepository = MemoryJokeRepository() 
+        for _ in range(3):
+            joke = joke_factory()
+            joke_repository.add_joke(joke)
+        get_joke_use_case = GetJoke(joke_repository)
