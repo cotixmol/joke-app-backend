a2d049f4092ab9a6c031fc2fc7996d66c03d7ee0
Constancio Molinengo <constancio.molinengo@outlook.com>
2024-01-05 13:42:44 -0300

    [COTI-JOKE] Joke memory repository updated

--- adapters/src/repositories/memory/memory_joke_repository.py
+++ adapters/src/repositories/memory/memory_joke_repository.py
@@ -1,17 +1,18 @@
-import random
 from core.src.repository import JokeRepository
 from core.src.models import Joke
 
+FIRST_ITEM = 0
+
+
 class MemoryJokeRepository(JokeRepository):
     def __init__(self):
-        self.jokes = [] 
+        self.jokes = []
 
     def get_joke(self):
         if self.jokes:
-            return random.choice(self.jokes)
+            return self.jokes[FIRST_ITEM]
         else:
-            return None 
-    
+            return None
+
     def add_joke(self, joke: Joke):
         self.jokes.append(joke)
-
