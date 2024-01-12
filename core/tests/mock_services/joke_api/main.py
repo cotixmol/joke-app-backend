cd6d49f4699ff2932bda664b3af3c9faa295e69c
Constancio Molinengo <constancio.molinengo@outlook.com>
2024-01-12 12:08:58 -0300

    [COTI-JOKES] Creation of mock jokes api with Docker

---
+++ core/tests/mock_services/joke_api/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+import json
+import random
+
+joke_app = FastAPI()
+
+with open('jokes.json', 'r') as file:
+    jokes = json.load(file)
+
+@joke_app.get("/joke")
+async def get_joke():
+    random_joke = random.choice(jokes)
+    return [random_joke]
